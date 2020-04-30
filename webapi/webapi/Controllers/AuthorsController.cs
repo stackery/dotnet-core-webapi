8f187604ea70d70d87eb87d6e8d668cded9fe22b
Chase Douglas <chase@stackery.io>
2020-04-30 15:34:10 -0700

    Add code!

---
+++ webapi/webapi/Controllers/AuthorsController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using webapi.Data;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : Controller
+    {
+        private webapiContext db = new webapiContext();
+
+        [HttpGet]
+        public IEnumerable<Author> GetAuthors()
+        {
+            return db.Authors;
+        }
+
+        // GET: api/Authors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            Author author = await db.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+
+        // PUT: api/Authors/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAuthor(int id, Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != author.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(author).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost("{id}")]
+        public async Task<ActionResult> PostAuthor(Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Authors.Add(author);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = author.Id }, author);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Author>> DeleteAuthor(int id)
+        {
+            Author author = await db.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            db.Authors.Remove(author);
+            await db.SaveChangesAsync();
+
+            return Ok(author);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return db.Authors.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ webapi/webapi/Data/webapiContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace webapi.Data
+{
+    public class webapiContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($"Data Source={Environment.GetEnvironmentVariable("DB_ADDRESS")},{Environment.GetEnvironmentVariable("DB_PORT")};Initial Catalog=books;User ID=root;Password=stacksonstacks");
+        }
+
+        public DbSet<webapi.Models.Author> Authors { get; set; }
+
+        public DbSet<webapi.Models.Book> Books { get; set; }
+    }
+}
